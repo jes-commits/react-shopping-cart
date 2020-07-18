dee00c647746c761c6bb626b92b60373fb03079b
sowah <justice.sowah18@gmail.com>
2020-07-18 23:10:34 +0000

    modified App.js and items.js

--- src/App.js
+++ src/App.js
@@ -1,116 +1,161 @@
-import React from 'react';
-import './App.css';
-import ActualSize from './sizes';
-import Item from './items';
-import Pix1 from './images/img1.jpg';
-import Pix2 from './images/img2.jpg';
-import Pix3 from './images/img3.jpg';
-import Pix4 from './images/img4.jpg';
-import Pix5 from './images/img5.jpg';
-import Pix6 from './images/img6.jpg';
-import Pix7 from './images/img7.jpg';
-import Pix8 from './images/img8.jpg';
-import Pix9 from './images/img9.jpg';
-import Pix10 from './images/img10.jpg';
-import Pix11 from './images/img11.jpg';
-import Pix12 from './images/img12.jpg';
-
+import React from "react";
+import "./App.css";
+import ActualSize from "./sizes";
+import Item from "./items";
+import Pix1 from "./images/img1.jpg";
+import Pix2 from "./images/img2.jpg";
+import Pix3 from "./images/img3.jpg";
+import Pix4 from "./images/img4.jpg";
+import Pix5 from "./images/img5.jpg";
+import Pix6 from "./images/img6.jpg";
+import Pix7 from "./images/img7.jpg";
+import Pix8 from "./images/img8.jpg";
+import Pix9 from "./images/img9.jpg";
+import Pix10 from "./images/img10.jpg";
+import Pix11 from "./images/img11.jpg";
+import Pix12 from "./images/img12.jpg";
 
 function App() {
-	return (
-		<div>
-			<div className='container'>
-				<div className='row'>
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          {/* left section begins from here*/}
 
-					{/* left section begins from here*/}
+        	<div className="col-2">
+            <h4 style={{ marginTop: "2vh" }}>Sizes:</h4>
+            <div className="row" style={{ marginBottom: "2vh" }}>
+              <div className="col-3">
+                <ActualSize name="XS" />
+              </div>
+              <div className="col-3">
+                <ActualSize name="S" />
+              </div>
+              <div className="col-3">
+                <ActualSize name="M" />
+              </div>
+              <div className="col-3">
+                <ActualSize name="ML" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3">
+                <ActualSize name="L" />
+              </div>
+              <div className="col-3">
+                <ActualSize name="XL" />
+              </div>
+              <div className="col-3">
+                <ActualSize name="XX" />
+              </div>
+            </div>
 
-					<div className='col-2'>
-						<h4 style={{ marginTop: '2vh' }}>Sizes:</h4>
-						<div style={{display: 'flex', justifyContent: 'space-evenly', marginBottom: '2vh', marginLeft: '-0.3vw', marginTop: '3vh'}}>
-							<ActualSize name='XS' />
-							<ActualSize name='S' />
-							<ActualSize name='M' />
-							<ActualSize name='ML' />
-						</div>
-						<div style={{display: 'flex', justifyContent: 'space-evenly', marginLeft: '-1.2vw'}}>
-							<ActualSize name='L' />
-							<ActualSize name='XL' />
-							<ActualSize name='XX' />
-						</div>
+            <p style={{ marginTop: "2vh" }}>
+              Leave a star on Github if this repository was useful :)
+            </p>
 
-						<p style={{marginTop: '2vh'}}>Leave a star on Github if this repository was useful :)</p>
+            <div style={{ display: "flex" }}>
+              <button
+                style={{
+                  fontWeight: "bold",
+                  marginRight: "0.5vw",
+                  border: "none",
+                  borderRadius: "10%",
+                  backgroundColor: "#ccd1d9",
+                }}
+              >
+                *Star
+              </button>
+              <button
+                style={{
+                  fontWeight: "bold",
+                  border: "none",
+                  borderRadius: "10%",
+                  backgroundColor: "#ccd1d9",
+                }}
+              >
+                848
+              </button>
+            </div>
+          </div>
 
-						<div style={{display: 'flex'}}>
-							<button style={{fontWeight: 'bold', marginRight: '0.5vw', border: 'none', borderRadius: '10%', backgroundColor: '#ccd1d9'}}>*Star</button>
-							<button style={{fontWeight: 'bold',border: 'none', borderRadius: '10%', backgroundColor: '#ccd1d9'}}>848</button>
-						</div>
-						
-					</div>
+          {/* right section begins from here*/}
 
+          <div className="col-10">
+            <div className="row">
+              <div className="col-6">
+                <h6 style={{ marginTop: "2vh" }}>16 Products found</h6>
+              </div>
+              <div className="col-6">
+                <div style={{ float: "right" }}>
+                  <div
+                    style={{
+                      display: "flex",
+					  float: "right",
+					  marginTop: '1vh'
+                    }}
+                  >
+                    <h6>Order by</h6>
+                    <select>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                      <option></option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-					{/* right section begins from here*/}
+            <div className="row">
+              <div className="col-3">
+                <Item pix={Pix1} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix2} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix3} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix4} />
+              </div>
+            </div>
 
-					<div className='col-10'>
-						<h6 style={{ marginTop: '2vh' }}>16 Products found</h6>
-						<div style={{ display: 'flex', float: 'right', marginTop: '-4vh' }}>
-							<h6>Order by</h6>
-							<select>
-								<option></option>
-								<option></option>
-								<option></option>
-								<option></option>
-								<option></option>
-							</select>
-
-						</div>
-						<div className='row'>
-							<div className='col-3'>
-								<Item pix={Pix1} />
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix2}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix3}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix4}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix5}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix6}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix7}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix12}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix8}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix9}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix10}/>
-							</div>
-							<div className='col-3'>
-								<Item pix={Pix11}/>
-							</div>
-						</div>
-
-
-					</div>
-
-				</div>
-
-			</div>
-		</div>
-	);
+            <div className="row">
+              <div className="col-3">
+                <Item pix={Pix5} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix6} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix7} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix12} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3">
+                <Item pix={Pix8} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix9} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix10} />
+              </div>
+              <div className="col-3">
+                <Item pix={Pix11} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
--- src/items.js
+++ src/items.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
-function Item({pix}) {
-    return(
-        <div>
-            <h6 style={{color: 'white', backgroundColor: 'black', textAlign: 'center', float: 'right', marginRight: '-3vw',height: '3vh'}}>Free Shipping</h6>
-            <img src={pix} alt='shirt one'/>
-            <h6 style={{fontWeight: 'bold', textAlign: 'center', marginTop: '1vh', marginLeft: '4vw'}}>Designer Wear</h6>
-            <hr style={{width: '5vw', height: '5px', backgroundColor: 'yellow', textAlign: 'center', marginLeft: '6vw'}} />
-            <p style={{textAlign: 'center', marginLeft: '4vw'}}>$<span style={{fontWeight: 'bold', fontSize: '26px'}}>10</span>.90</p>
-            <p style={{textAlign: 'center', marginLeft: '4vw'}}>or 9*$1.21</p>
-            <button style={{border: 'none', marginBottom: '5vh',marginLeft: '2vw', backgroundColor:'black', color:'white', textAlign: 'center', width:'13vw', height: '5vh', borderRadius: '5px'}}>Add to cart</button>
+function Item({ pix }) {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div class="card" style={{ width: '35', marginBottom: '1.5vh', height: '75vh' }}>
+                    <h6 style={{color: 'white', backgroundColor: 'black', height: '3vh', width: '8vw', textAlign: 'center',marginLeft: '7.5vw'}}>Free Shipping</h6>
+                    <img src={pix} className="card-img-tops" alt='shirt one' />
+                    <div className="card-body">
+                        <h5 className="card-title" style={{ textAlign: 'center'}}>Designer Wear</h5>
+                        <hr style={{ width: '5vw', height: '5px', backgroundColor: 'yellow', textAlign: 'center' }} />
+                        <p style={{ textAlign: 'center'}}>$<span style={{ fontWeight: 'bold', fontSize: '26px' }}>10</span>.90</p>
+                        <p style={{ textAlign: 'center'}}>or 9*$1.21</p>
+                        <button style={{ border: 'none', marginBottom: '5vh', backgroundColor: 'black', color: 'white', textAlign: 'center', width: '12.5vw', height: '5vh', borderRadius: '5px' }}>Add to cart</button>
+                    </div>
+                </div>
+
+            </div>
+
+
+
         </div>
     )
 }
 
 export default Item;
+
